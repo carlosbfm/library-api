633abd13498a91e458afcc58256fe29310d6a5eb
Carlos-Manoel-2005 <carlosmanoelbfm@gmail.com>
2026-03-26 08:47:08 -0300

    chore: adiciona classe principal, properties e pasta de testes

---
+++ src/main/java/library_api/LibraryApiApplication.java
@@ -0,0 +1,16 @@
+package library_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class LibraryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApiApplication.class, args);
+    }
+
+}
