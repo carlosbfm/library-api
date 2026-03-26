f28738abaf18d07efaa3be0ba88cb475867d2816
Carlos-Manoel-2005 <carlosmanoelbfm@gmail.com>
2026-03-26 08:30:52 -0300

    feat: cria entidade de livro

---
+++ src/main/java/library_api/model/entity/livro/LivroEntity.java
@@ -0,0 +1,49 @@
+package library_api.model.entity.livro;
+
+
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import library_api.model.enums.StatusLivro;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "livro")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LivroEntity {
+
+    @Id
+    @Column( name = "codigo_livro", length = 6,nullable = false, unique = true )
+    private Long codLivro;
+
+    @Column(name = "titulo", length = 150)
+    private String tituloLivro;
+
+    @Column(name = "autor",length = 100)
+    private String autor;
+
+    @Column(name = "genero", length = 50)
+    private String genero;
+
+    @Column(name = "isbn", length = 20)
+    private String isbn;
+
+    @Column(name = "data_lancamento")
+    private LocalDate data;
+
+    @Column(name = "status", length = 100)
+    private StatusLivro status;
+
+}
